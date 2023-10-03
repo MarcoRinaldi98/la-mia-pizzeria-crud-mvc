94fa2b87041cf84b70297b964f90a45f43558752
Marco Rinaldi <marcorinaldi98@libero.it>
2023-10-03 12:38:11 +0200

    Fine delle CRUD con aggiunta modifica e eliminazione delle pizze

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using la_mia_pizzeria_static.Models;
 using la_mia_pizzeria_static.Database;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace la_mia_pizzeria_static.Controllers
 {
@@ -54,6 +55,77 @@ namespace la_mia_pizzeria_static.Controllers
                 db.SaveChanges();
 
                 return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza? pizzaToEdit = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaToEdit == null)
+                {
+                    return NotFound("La pizza che vuoi modificare non è stata trovata");
+                }
+                else
+                {
+                    return View("Update", pizzaToEdit);
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza modifiedPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Update", modifiedPizza);
+            }
+
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza? pizzaToUpdate = db.Pizze.Find(id);
+
+                if (pizzaToUpdate != null)
+                {
+                    EntityEntry<Pizza> entryEntity = db.Entry(pizzaToUpdate);
+                    entryEntity.CurrentValues.SetValues(modifiedPizza);
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("Mi dispiace non è stata trovata la pizza da aggiornare");
+                }
+
+
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza? pizzaToDelete = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaToDelete != null)
+                {
+                    db.Pizze.Remove(pizzaToDelete);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("La pizza da eliminare non è stata trovata!");
+                }
             }
         }
     }
